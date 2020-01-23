4abac05a0be420d1b4d577e180fe3c5d7a6de676
sys admin <develop@pasedb.org>
2020-01-23 00:55:58 +0000

    updated source for DataMnager

--- lesson8/src/main/java/com/north/webapp/DataManager.java
+++ lesson8/src/main/java/com/north/webapp/DataManager.java
@@ -6,7 +6,7 @@ public class DataManager {
 	private Connection conn = null;
 	
 
-	public DataManager() {
+	public DataManager(boolean doTest) {
 		// TODO Auto-generated constructor stub
 	
 			// TODO Auto-generated constructor stub
@@ -66,9 +66,6 @@ public class DataManager {
 
 	}
 */
-	public static void main(String args[]) {
-		new DataManager();
-	}
 
 	public void login(String username, String password) throws Exception {
 		System.out.println("login");
@@ -101,14 +98,25 @@ public class DataManager {
 		StringBuffer html = new StringBuffer();
 		try {
 			openConnection();
+
+			Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			ResultSet rs = stmt.executeQuery("SELECT name, url FROM menu");
+			while(rs.next()) {
+				System.out.println(rs.getString("url") + ":" + rs.getString("name"));
+			}
+			
+			//html.append("<a class=\"pureCssMenui\" href=\"" + rs.getString("URL") + "\">" + rset.getString("name")
+				//	+ "</a>");
 		} catch (Exception ex) {
-			StringBuffer result = new StringBuffer();
-			return result.toString();
+			//StringBuffer result = new StringBuffer();
+			//return result.toString();
+			ex.printStackTrace();
 		} finally {
 			closeConnection();
 		}
 		return html.toString();
 	}
+
 	
 	private void closeConnection() {
 		try {
@@ -126,7 +134,17 @@ public class DataManager {
 			
 			Class.forName("com.mysql.jdbc.Driver");  
 			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/glm","timmy","thegoat");
+			System.out.println("Connection successfully opened: " + !conn.isClosed());
 			
 	}
 
+	public static void main(String args[]) {
+		DataManager dm = null;
+		try{
+			dm = new DataManager();
+			dm.fetchMenus();
+		}catch(Exception ex){
+			dm.closeConnection();
+		}	
+	}
 }
