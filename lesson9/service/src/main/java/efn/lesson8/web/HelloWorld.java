f599238e83fae7e831f0bc7336ccc3b06744ca78
Trasely <david.a.thomas1003@gmail.com>
2020-02-03 16:52:48 -0600

    redo

---
+++ lesson9/service/src/main/java/efn/lesson8/web/HelloWorld.java
@@ -0,0 +1,20 @@
+package efn.lesson8.web;
+
+
+
+public class HelloWorld {
+	
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String sayHello() { return "Hello World!"; }
+	
+	public static void main(String[] args) {
+		System.out.println(new HelloWorld().sayHello());
+	}
+	
+	
+	
+	
+}
