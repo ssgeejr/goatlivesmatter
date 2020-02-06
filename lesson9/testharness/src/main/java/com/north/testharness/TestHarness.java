13668e18fbc7ca549bc028d61c350f20f0e40c11
ioexcept <ioexcept@gmail.com>
2020-02-06 09:05:09 -0600

    updateing lesson9 test harness

--- lesson9/testharness/src/main/java/com/north/testharness/TestHarness.java
+++ lesson9/testharness/src/main/java/com/north/testharness/TestHarness.java
@@ -13,7 +13,7 @@ public class TestHarness {
 			//this is a guess ... try it out, get it to work - then move this code (google how to include your imports) and add it to your index.jsp (and others)
 			// once this works - it's all over except dockerizing it and building it with Jenkins
 			
-			URL oracle = new URL("http://localhost:8088/webapp/Menu.jsp");
+			URL oracle = new URL("http://localhost:8088/service/Menu.jsp");
 	        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
 	        String inputLine;
 	        while ((inputLine = in.readLine()) != null)
